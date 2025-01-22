7570529d6e2011631ccbd310dd8f9dade8c64994
testblah <testblah@gmail.com>
2025-01-22 08:25:58 -0800

    ds

--- filebeat/make.bat
+++ filebeat/make.bat
@@ -142550,6 +142550,136 @@ IF %ERRORLEVEL% NEQ 0 go get github.com/magefile/mage
 mage %*
 @echo off
 fsdaf
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this once you may invoke mage.exe directly.
+
+WHERE mage
+IF %ERRORLEVEL% NEQ 0 go get github.com/magefile/mage
+
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this on
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this once you may invoke mage.exe directly.
+
+WHERE mage
+IF %ERRORLEVEL% NEQ 0 go get github.com/magefile/mage
+
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this on
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this once you may invoke mage.exe directly.
+
+WHERE mage
+IF %ERRORLEVEL% NEQ 0 go get github.com/magefile/mage
+
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this on
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this once you may invoke mage.exe directly.
+
+WHERE mage
+IF %ERRORLEVEL% NEQ 0 go get github.com/magefile/mage
+
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this on
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this once you may invoke mage.exe directly.
+
+WHERE mage
+IF %ERRORLEVEL% NEQ 0 go get github.com/magefile/mage
+
+mage %*
+@echo off
+fsdaf
+REM Windows wrapper for Mage (h
+
+
+
+
+REM Windows wrapper for Mage (https://magefile.org/) that installs it
+REM to %GOPATH%\bin from the Beats vendor directory.
+REM
+REM After running this on
 REM Windows wrapper for Mage (h
 
 
